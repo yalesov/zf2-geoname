ff352a21fa1bb38efd81988858bf1dd3ea7249d0
heartsentwined <heartsentwined@cogito-lab.com>
2012-09-27 03:59:05 +0800

    exceptions

---
+++ src/Heartsentwined/Geoname/Exception/RuntimeException.php
@@ -0,0 +1,16 @@
+<?php
+namespace Heartsentwined\Geoname\Exception;
+
+use Heartsentwined\Geoname\ExceptionInterface;
+
+/**
+ * RuntimeException
+ *
+ * @author heartsentwined <heartsentwined@cogito-lab.com>
+ * @license GPL http://opensource.org/licenses/gpl-license.php
+ */
+class RuntimeException
+    extends \RuntimeException
+    implements ExceptionInterface
+{
+}
---
+++ src/Heartsentwined/Geoname/ExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Heartsentwined\Geoname;
+
+/**
+ * ExceptionInterface
+ *
+ * @author heartsentwined <heartsentwined@cogito-lab.com>
+ * @license GPL http://opensource.org/licenses/gpl-license.php
+ */
+interface ExceptionInterface
+{
+}
